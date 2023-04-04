5d4f76b6bfeba05c46069b52bb70efd93bd4c789
Vani Goel <vani140103@gmail.com>
2023-04-04 19:55:23 +0530

    view engine added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,11 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Express is up!!</h1>');
+
+    //before ejs file
+    // return res.end('<h1>Express is up!!</h1>');
+
+    return res.render('home',{
+        title: "Vani"
+    });
 };
 
 //module.exports.actionName = function(req,res){}
