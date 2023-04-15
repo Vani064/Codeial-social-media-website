49bff5d8434d5161e5c9229859fb112b289cde58
Vani Goel <vani140103@gmail.com>
2023-04-15 12:45:30 +0530

    user authenticated for posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const postcontroller = require('../controllers/posts_controller');
 
-router.post('/create',postcontroller.create);
+router.post('/create',passport.checkAuthentication,postcontroller.create);
 
 module.exports = router;
