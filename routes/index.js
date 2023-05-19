9ac32ef882473ffcc8fe0ab40f98f6493dd28f01
Vani Goel <vani140103@gmail.com>
2023-05-19 18:28:27 +0530

    creating version folder and sending api requests

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,8 @@ router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
 
 router.use('/comment',require('./comments'));
+
+router.use('/api', require('./api'));
 //for any further routes, access from here
 // router.use('/routername',require('./routerfile'));
 
