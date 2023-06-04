b178e747b0e71c77b5d719a1a54752c930cd07d2
Vani064 <77920882+Vani064@users.noreply.github.com>
2023-06-04 14:42:24 +0530

    deployment ready

--- config/environment.js
+++ config/environment.js
@@ -66,4 +66,4 @@ const production = {
 
 
 
-module.exports = development;
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
